8527fd2f71533f75320fd4b8309cfd9f03797ae6
Adam Howell <AdamJHowell@users.noreply.github.com>
2023-12-27 18:02:47 -0700

    Changed the loop to be timed, instead of delayed.

--- M5AtomPbHubQuadLight.ino
+++ M5AtomPbHubQuadLight.ino
@@ -1,11 +1,15 @@
-/*
+/**
  * Description: Use PbHUB to read the analog input values.
-*/
+ */
 
 #include <M5Atom.h>
 #include "porthub.h"
 
 PortHub porthub;
+unsigned long lastLoop = 0;                        // The time when the most recent loop completed.
+unsigned long loopCount = 0;                       // The number of loops which have occurred.
+const unsigned long loopDelay = 10;                // The maximum value of 4,294,967,295 allows for a delay of about 49.7 days.
+const char * sketchName = "M5AtomPbHubQuadLight";  // The name of this sketch.
 // Addresses of the six ports: HUB1_ADDR, HUB2_ADDR, HUB3_ADDR, HUB4_ADDR, HUB5_ADDR, HUB6_ADDR
 
 
@@ -19,12 +23,18 @@ void setup()
 void loop() 
 {
 	M5.update();
-  uint16_t port1 = porthub.hub_a_read_value( HUB1_ADDR );
-  uint16_t port2 = porthub.hub_a_read_value( HUB2_ADDR );
-  uint16_t port5 = porthub.hub_a_read_value( HUB5_ADDR );
-  uint16_t port6 = porthub.hub_a_read_value( HUB6_ADDR );
-  // To read the digital values
-  uint8_t port1da porthub.hub_d_read_value_A( HUB1_ADDR );
-  uint8_t port1db porthub.hub_d_read_value_B( HUB1_ADDR );
-  Serial.printf( "L0:%d L1:%d L4:%d L5:%d\n", port1, port2, port5, port6 );	
+	unsigned long time = millis();
+	if( ( lastLoop == 0 ) || ( time - lastLoop ) > loopDelay )
+	{
+		loopCount++;
+    uint16_t port1 = porthub.hub_a_read_value( HUB1_ADDR );
+    uint16_t port2 = porthub.hub_a_read_value( HUB2_ADDR );
+    uint16_t port5 = porthub.hub_a_read_value( HUB5_ADDR );
+    uint16_t port6 = porthub.hub_a_read_value( HUB6_ADDR );
+    // Read the digital values for A and B pins.
+    uint8_t port1da porthub.hub_d_read_value_A( HUB1_ADDR );
+    uint8_t port1db porthub.hub_d_read_value_B( HUB1_ADDR );
+    Serial.printf( "L0:%d L1:%d L4:%d L5:%d\n", port1, port2, port5, port6 );
+		lastLoop = millis();
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Uses the M5Stack PbHUB to read analog values from four M5Stack LIGHT sensors.
+This project reads light levels from four analog sensors.
+
+Specifically, it is an implementation of the [M5Stack PbHUB](https://shop.m5stack.com/products/i-o-hub-1-to-6-expansion-unit-stm32f0) to read analog values from four [M5Stack LIGHT sensors](https://docs.m5stack.com/en/unit/LIGHT).
+This project is the basis for a Lego sun tracker.
+The tracker will use the [M5Stack ATOM Motion base](https://docs.m5stack.com/en/atom/atom_motion) along with two [Kittenbot Geekservo continuous rotation servos](https://www.aliexpress.us/item/3256802685917966.html).
+
+Uses the [M5Stack Pb.HUB library](https://github.com/m5stack/M5Stack/tree/master/examples/Unit/PbHUB) to simplify managing the hardware.
+
+This has been tested on the [M5Stack Atom Lite](https://docs.m5stack.com/en/core/atom_lite) and works perfectly.
